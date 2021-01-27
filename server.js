d0777aba317036991957807e539dc550adc10d48
Sammy Kroner <sammyk118@gmail.com>
2021-01-26 22:23:10 -0800

    consolidated server js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@
 // ==============================================================================
 
 var express = require("express");
-
+var path = require("path");
+const fs = require("fs");
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -19,6 +20,9 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
+
+
 
 // ================================================================================
 // ROUTER
@@ -26,8 +30,43 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+var noteData = []
+
+
+
+
+
+fs.readFile("db.json", function (err, data) {
+
+})
+
+app.get("/api/notes", function (req, res) {
+  res.json(noteData);
+});
+
+
+app.post("/api/notes", function (req, res) {
+  console.log(req.body);
+  var data = req.body;
+  noteData.push(req.body);
+
+});
+
+app.delete("/api/clear", function (req, res) {
+  console.log("this will delete things");
+});
+
+
+
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 
 // var apiRoutes = require("./routes/apiRoutes")
 // console.log(apiRoutes); //=> [Function]
@@ -38,6 +77,6 @@ require("./routes/htmlRoutes")(app);
 // The below code effectively "starts" our server
 // =============================================================================
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
